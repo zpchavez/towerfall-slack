ab61d43728abcca6e5d8c4b9aba79d68b90e1fed
Zachary Chavez <zach@rychavez.com>
2015-10-04 16:33:13 -0700

    Use save-to-db branch of tf-stats and share watch and db-config commands

--- bin/watch-stats.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-var fileHandler = require('towerfall-stats').fileHandler;
-
-var append = ['-a', '--append'].indexOf(process.argv[2]) !== -1;
-
-console.log('Watching for stat updates');
-fileHandler.watchForUpdatesAndSaveToFile(append);
