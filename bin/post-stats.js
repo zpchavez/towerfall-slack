484c1fc50c264398ccd34b8652c24da1a24299ad
Zachary Chavez <zach@rychavez.com>
2016-04-10 10:13:50 -0700

    Don't JSON-decode non-JSON responses

--- bin/post-stats.js
+++ bin/post-stats.js
@@ -4,6 +4,7 @@ var fileHandler = require('towerfall-stats').fileHandler;
 var slackPoster = require('../lib/slack-poster');
 var config      = require('towerfall-stats').config;
 var fetch       = require('node-fetch');
+var isJsonResponse = require('../lib/is-json-response');
 
 // Post stats from the last 6 hours.
 
@@ -15,23 +16,29 @@ formattedDate = formattedDate.replace(/\.\d+Z$/, 'Z');
 fetch(
     `${config.apiUrl}/group/${config.groupId}/summary?start=${formattedDate}&rankings&table`
 ).then(response => {
-    response.json().then(json => {
-        if (response.ok) {
-            if (parseInt(json.matches, 10) !== 0) {
-                return slackPoster.postStats(json)
-                    .then(() => {
-                        process.exit(0);
-                    });
-            } else {
-                console.log('No stats to post');
+    if (! isJsonResponse(response)) {
+        return response.text().then(text => [text, response]);
+    } else {
+        return response.json().then(json => [json, response]);
+    }
+}).then(contentAndResponse => {
+    var content = contentAndResponse[0];
+    var response = contentAndResponse[1];
+    if (response.ok) {
+        if (parseInt(content.matches, 10) !== 0) {
+            return slackPoster.postStats(content)
+            .then(() => {
                 process.exit(0);
-            }
+            });
         } else {
-            console.log(json);
-            process.exit(1);
+            console.log('No stats to post');
+            process.exit(0);
         }
-    }).catch(err => {
-        console.log(err, err.stack);
+    } else {
+        console.log(content);
         process.exit(1);
-    });
+    }
+}).catch(err => {
+    console.log(err, err.stack);
+    process.exit(1);
 });
