b848119e2649dbe638009712a86244d0b25041b5
Zachary Chavez <zach@rychavez.com>
2016-04-10 10:49:09 -0700

    Allow -t option for type and --teams to get team stats

--- bin/post-stats.js
+++ bin/post-stats.js
@@ -8,14 +8,20 @@ var isJsonResponse = require('../lib/is-json-response');
 var argv = require('minimist')(process.argv.slice(2));
 
 var hoursBack = parseInt(argv.h, 10) || 6;
+var type = argv.t;
+var typeString = type ? `&type=${type}` : '';
+var teams = argv.teams;
 
 var since = new Date(Date.now() - hoursBack * 60 * 60 * 1000);
 var formattedDate = since.toJSON();
 // API doesn't support fractional seconds, so get rid of those
 formattedDate = formattedDate.replace(/\.\d+Z$/, 'Z');
 
+var path = `/group/${config.groupId}/${teams ? 'team-summary' : 'summary'}`;
+var queryString = `?start=${formattedDate}&rankings&table${typeString}`;
+
 fetch(
-    `${config.apiUrl}/group/${config.groupId}/summary?start=${formattedDate}&rankings&table`
+    `${config.apiUrl}${path}${queryString}`
 ).then(response => {
     if (! isJsonResponse(response)) {
         return response.text().then(text => [text, response]);
@@ -27,7 +33,19 @@ fetch(
     var response = contentAndResponse[1];
     if (response.ok) {
         if (parseInt(content.matches, 10) !== 0) {
-            return slackPoster.postStats(content)
+            var headingText;
+            switch (type) {
+                case 'ffa':
+                    headingText = '*Free-for-all Stats*';
+                    break;
+                case '2v2':
+                    headingText = '*2v2 Stats*';
+                    break;
+                default:
+                    headingText = teams ? '*2v2 Stats*' : '*Overall Stats*';
+                    break;
+            }
+            return slackPoster.postStats(content, headingText)
             .then(() => {
                 process.exit(0);
             });
