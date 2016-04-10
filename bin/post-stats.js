a4fce2724ea922e65672f0e7fde9d7a2b4284fbb
Zachary Chavez <zach@rychavez.com>
2016-04-09 17:29:09 -0700

    Update post-stats to get data from the API

--- bin/post-stats.js
+++ bin/post-stats.js
@@ -1,22 +1,37 @@
 #!/usr/bin/env node
 'use strict';
-var fs          = require('fs');
 var fileHandler = require('towerfall-stats').fileHandler;
 var slackPoster = require('../lib/slack-poster');
 var config      = require('towerfall-stats').config;
+var fetch       = require('node-fetch');
 
-var liveStatsFile = config.liveStatsFile;
+// Post stats from the last 6 hours.
 
-fs.exists(liveStatsFile, function(exists) {
-    if (! exists) {
-        console.log('Live stats file not found. Run watch-stats and play a match to create it.');
-    } else {
-        var stats = fileHandler.getCompiledLiveStats();
+var sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
+var formattedDate = sixHoursAgo.toJSON();
+// API doesn't support fractional seconds, so get rid of those
+formattedDate = formattedDate.replace(/\.\d+Z$/, 'Z');
 
-        slackPoster.postStats(stats, function() {
-            console.log('Stats posted');
-            // Delete file so we can start a new one
-            fs.unlink(liveStatsFile);
-        });
-    }
+fetch(
+    `${config.apiUrl}/group/${config.groupId}/summary?start=${formattedDate}&rankings&table`
+).then(response => {
+    response.json().then(json => {
+        if (response.ok) {
+            if (parseInt(json.matches, 10) !== 0) {
+                return slackPoster.postStats(json)
+                    .then(() => {
+                        process.exit(0);
+                    });
+            } else {
+                console.log('No stats to post');
+                process.exit(0);
+            }
+        } else {
+            console.log(json);
+            process.exit(1);
+        }
+    }).catch(err => {
+        console.log(err, err.stack);
+        process.exit(1);
+    });
 });
